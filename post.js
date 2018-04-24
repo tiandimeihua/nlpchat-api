6754fb3d5b5e59cabcacce0ae141a185deeb9e9b
tiandimeihua <25058977+tiandimeihua@users.noreply.github.com>
2018-04-24 16:20:33 -0400

    feat(*): update post.js with input words bug;
    the sign does work now, but when input words have "?", the function will go over. I will tey to fix
    it.

---
+++ post.js
@@ -0,0 +1,110 @@
+"use strict";
+
+const _ = require("underscore");
+const md5 = require("blueimp-md5");
+const axios = require("axios");
+const qs = require("qs");
+
+//--------------input your own information here-----------------------;
+// set your appID and appKey;
+let appID = 1106775473;
+let appKey = "v1irTeNouD9mqPO1";
+
+//--------------input your words here---------------------------------;
+// set the words;
+let words = "你好";
+
+//--------------------------------------------------------------------;
+//set hashes list;
+
+let hash = [];
+
+let randomString =
+  Math.random()
+    .toString(36)
+    .substring(2, 10) +
+  Math.random()
+    .toString(36)
+    .substring(2, 10) +
+  Math.random()
+    .toString(36)
+    .substring(2, 10) +
+  Math.random()
+    .toString(36)
+    .substring(2, 10);
+
+hash["app_id"] = appID; //int >0;
+hash["time_stamp"] = Math.round(new Date().getTime() / 1000); //int >0;
+hash["nonce_str"] = randomString; //string upTo 32 bytes;
+hash["sign"] = ""; //string 32 bytes;
+hash["session"] = "nlpChat-api"; //string UTF-8, not null, upTo 32 bytes;
+hash["question"] = words; //string UTF-8, not null, upTo 300 bytes;
+
+//console.log(hash);
+
+//--------------------------------------------------------------------;
+//get sign function;
+
+function getSign(hash, appKey) {
+  //--------------------------------------------------------------------;
+  //1.re-order the hash list;
+  let reOrderHash = _.pairs(hash).sort();
+  //console.log(reOrderHash);
+
+  //--------------------------------------------------------------------;
+  //2.add URLstring;
+  let string = "";
+  for (let index = 0; index < reOrderHash.length; index++) {
+    if (reOrderHash[index][1] !== "") {
+      if (reOrderHash[index][1] == encodeURIComponent(reOrderHash[index][1])) {
+        string =
+          string + reOrderHash[index][0] + "=" + reOrderHash[index][1] + "&";
+      } else {
+        string =
+          string +
+          reOrderHash[index][0] +
+          "=" +
+          encodeURIComponent(reOrderHash[index][1]) +
+          "&";
+      }
+    }
+  }
+
+  string = string.replace("%20", "+");
+  //console.log(string);
+
+  //--------------------------------------------------------------------;
+  //3.add app_key;
+  string = string + "app_key=" + appKey;
+  //console.log(string);
+
+  //--------------------------------------------------------------------;
+  //4.MD5+uppercase;
+  let sign = md5(string).toUpperCase();
+  //console.log(sign);
+
+  return sign;
+}
+
+hash["sign"] = getSign(hash, appKey);
+//console.log(hash);
+
+//--------------------------------------------------------------------;
+//do post;
+
+let options = {
+  method: "POST",
+  headers: { "content-type": "application/x-www-form-urlencoded" },
+  data: qs.stringify(hash),
+  url: "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textchat"
+};
+
+//console.log(options.data);
+
+axios(options)
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
